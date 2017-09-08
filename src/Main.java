427e14a54dcbb4f53185a74baab0cd036492b1c7
saikumar1232 <31738794+saikumar1232@users.noreply.github.com>
2017-09-08 16:29:59 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		
+         Scanner sc=new Scanner(System.in);
+         int n=sc.nextInt();
+        int[][] arr=new int[n][4] 
+        for(int i=0;i<n;i++)
+        {
+        int id=sc.nextInt(); 
+        String fullname=sc.next();
+        date birthdate=new Date();
+        double avgmark=sc.nextDouble();
+        }
+
+}
